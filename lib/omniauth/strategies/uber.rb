2f7540337e6434327b2314000bd0d80c3f419b54
Tom Milewski <tmilewski@gmail.com>
2014-08-20 15:39:00 -0400

    Retrieve information about the user

--- lib/omniauth/strategies/uber.rb
+++ lib/omniauth/strategies/uber.rb
@@ -5,24 +5,28 @@ module OmniAuth
     class Uber < OmniAuth::Strategies::OAuth2
       DEFAULT_SCOPE = 'profile'
 
-      option :client_options, :site          => 'https://login.uber.com',
-                              :authorize_url => '/oauth/authorize',
-                              :token_url     => '/oauth/token'
+      option :client_options, :site          => 'https://api.uber.com',
+                              :authorize_url => 'https://login.uber.com/oauth/authorize',
+                              :token_url     => 'https://login.uber.com/oauth/token'
 
-      uid { nil }
+      uid { raw_info['uuid'] }
 
       info do
-        # Not implemented yet
-        {}
+        {
+          :first_name => raw_info['first_name'], 
+          :last_name  => raw_info['last_name'],
+          :email      => raw_info['email'],
+          :picture    => raw_info['picture'],
+          :promo_code => raw_info['promo_code']
+        }
       end
 
       extra do
-        # Not implemented yet
-        {}
+        { :raw_info => raw_info }
       end
 
       def raw_info
-        @raw_info ||= access_token.params['user'] || {}
+        @raw_info ||= access_token.get("/v1/me").parsed || {}
       end
 
       def request_phase
